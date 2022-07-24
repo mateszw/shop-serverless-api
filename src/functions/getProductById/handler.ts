e6ba1a4f10bea2b0b18d24cf638641448412ae4b
mateszw <mateuszs8892@gmail.com>
2022-07-24 14:38:52 +0200

    task-4 completed

--- src/functions/getProductById/handler.ts
+++ src/functions/getProductById/handler.ts
@@ -1,24 +1,32 @@
-import PRODUCTS_MOCK from "@functions/__mocks__/productsMock";
 import { middyfy } from "@libs/lambda";
+import { APIGatewayEvent } from "@models/apiGatewayEvent.model";
+import ProductService from "@services/product.service";
 
-export const getProductById = async (event) => {
-  const product = PRODUCTS_MOCK.find(
-    (product) => product.id === event.pathParameters.id
-  );
-
-  if (!product) {
+export const getProductById: APIGatewayEvent = async (event) => {
+  try {
+    const product = await new ProductService().getProductById(
+      event.pathParameters.id
+    );
+    if (!product) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          message: "Product with given id does not exists",
+        }),
+      };
+    }
     return {
-      statusCode: 404,
+      statusCode: 200,
+      body: JSON.stringify(product),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
       body: JSON.stringify({
-        message: "Product with given id does not exists",
+        message: "Internal Server Error",
       }),
     };
   }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(product),
-  };
 };
 
 export const main = middyfy(getProductById);
--- src/functions/getProductList/handler.ts
+++ src/functions/getProductList/handler.ts
@@ -1,11 +1,23 @@
 import { middyfy } from "@libs/lambda";
-import PRODUCTS_MOCK from "@functions/__mocks__/productsMock";
+import { APIGatewayEvent } from "@models/apiGatewayEvent.model";
+import ProductService from "@services/product.service";
 
-export const getProductList = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(PRODUCTS_MOCK),
-  };
+export const getProductList: APIGatewayEvent = async () => {
+  try {
+    const products = await new ProductService().getAllProducts();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(products),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Internal Server Error",
+      }),
+    };
+  }
 };
 
 export const main = middyfy(getProductList);
